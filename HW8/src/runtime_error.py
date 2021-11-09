b604a1de9388a71a2eadfd570d93fc1ec2ce4442
grad-kushal <ksk7657@g.rit.edu>
2021-11-09 22:34:17 +0530

    First commit HW8

---
+++ HW8/src/runtime_error.py
@@ -0,0 +1,12 @@
+"""
+CSCI-603 Parser Lab
+Author: RIT CS
+
+A custom exception class for representing a runtime error.
+"""
+
+
+class RuntimeError(Exception):
+
+    def __init__(self, message):
+        super.__init__(message)
